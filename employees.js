856e3f8e7125393beed94bb32e9f7e58f73a9314
yasmin yazdi <yyazdi13@gmail.com>
2020-01-13 23:24:44 -0600

    able to search employee, update, remove, and add

---
+++ employees.js
@@ -0,0 +1,262 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var cTable = require('console.table');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employee_tracker_db"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId + "\n");
+    userPrompt();
+});
+
+function userPrompt(){
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "start",
+            message: "what would you like to do?",
+            choices: ["view all employees", "view all employees by manager","add employee", 
+            "update employee", "remove employee", "update employee role", "update employee manager"]
+        }
+    ]).then(function(res){
+        if (res.start === "view all employees"){
+            allEmployees();
+        }
+        else if (res.start === "view all employees by manager"){
+            EmployeeByManagers();
+        }
+        else if (res.start === "add employee"){
+            addEmployee();
+        }
+        else if (res.start === "update employee"){
+            updateEmployee();
+        }
+        else if (res.start === "remove employee"){
+            removeEmployee();
+        }
+    })
+}
+
+function allEmployees(){
+    connection.query("SELECT * FROM employee", function(err, res){
+        if (err){
+            console.log(err)
+        }
+        console.table(res)
+    })
+}
+
+function EmployeeByManagers(){
+    connection.query(`SELECT e.first_name employeeFirst, e.last_name employeeLast,
+                            m.first_name managerFirst, m.last_name managerLast
+                    FROM employee e
+                    LEFT JOIN employee m ON m.manager_id = e.emp_id`, function(err, res){
+        if (err){
+            console.log(err);
+        }
+        console.table(res);
+    })
+};
+
+function addEmployee(){
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "has manager?",
+            choices: ["yes", "no"],
+            name: "manager"
+        },
+        {
+            type: "input",
+            message: "role title?",
+            name: "role"
+        },
+        {
+            type: "input",
+            message: "first name?",
+            name: "firstName" 
+        },
+        {
+            type: "input",
+            message: "last name?",
+            name: "lastName"
+        }
+    ]).then(function(res){
+
+        connection.query("SELECT r_id, title FROM employee INNER JOIN role ON role_id = r_id", function(err, data){
+            if (err) throw err; 
+            // console.log(res.role);
+            for (let i =0; i < data.length; i++){
+                if(res.role === data[i].title){
+                    var role = data[i].r_id;
+                }
+            }
+            addEmp(role,res);
+        })
+    
+        // var department = res.department;
+        // addDepartment(department)
+    })
+};
+
+
+
+// function addRole(role,res){
+//     connection.query("SELECT r_id, name, department_id FROM role INNER JOIN department ON department_id = dep_id WHERE ?",
+//     {
+//         r_id: role
+//     },
+//     function(err,data){
+//         if (err) throw err;
+//         console.log(data[0].department_id)
+//         addEmp(role,res);
+//     })
+// }
+
+function addEmp(role,res){
+    // if (res.manager === "yes"){
+        console.log(role + " role id")
+    //     inquirer.prompt([
+    //         {
+    //             type: "input",
+    //             message: "manager's last name?",
+    //             name: "managerLast"
+    //         }
+    //     ]).then(function(response){
+    //         connection.query("SELECT manager_id FROM employee WHERE ?",
+    //         {
+    //             last_name: response.managerLast
+    //         },
+    //         function(err, data){
+    //             if (err) throw err;
+    //             var manager = data[0].manager_id;
+    //         }
+    //         )
+
+    //     }).then(function(manager){
+    //         connection.query("INSERT INTO employee SET ?", 
+    //         [
+    //             {
+    //                 first_name: res.firstName
+    //             },
+    //             {
+    //                 last_name: res.lastName
+    //             },
+    //             {
+    //                 role_id: role
+    //             },
+    //             {
+    //                 manager_id: manager
+    //             },
+    //             ],
+    //             function(err){
+    //                 if (err) throw err;
+    //                 console.log("successfully added " + res.firstName + " " + res.lastName + "with a role id of " + role + 
+    //                 " and amanager id of " + manager);
+    //             }
+    //         )
+    //     })
+    // }
+    
+    connection.query("INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)", 
+    [res.firstName, res.lastName, role],
+    function(err,data){
+        if (err) throw err;
+        console.log("successfully added employee!")
+    });
+};
+
+function updateEmployee(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first',
+            message: "first name of the employee you wish to update?"
+        },
+        {
+            type: "input",
+            name: 'last',
+            message: "last name of the employee you wish to update?"
+        },
+        {
+            type: 'input',
+            name: 'firstChange',
+            message: "what would you like to change the first name to?"
+        },
+        {
+            type: "input",
+            name: 'lastChange',
+            message: "what would you like to change the last name to?"
+        }
+    ]).then(function(response){
+        connection.query('UPDATE employee SET ? WHERE ? ', 
+        [
+            {
+                first_name: response.firstChange
+            },
+            {
+                last_name: response.last
+            }
+        ],
+        function (err){
+            if (err){console.log(err)}
+        }
+        )
+        connection.query("UPDATE employee SET ? WHERE ?", [
+            {
+                last_name: response.lastChange
+            },
+            {
+                first_name: response.first
+            }
+        ], function(err){
+            if (err){console.log(err);}
+        });
+    });
+};
+
+function removeEmployee(){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "employee first name?",
+            name: "first_name"
+        },
+        {
+            type: "input",
+            message: "employee last name?",
+            name: "last_name"
+        }
+    ]).then(function(response){
+        var first = JSON.stringify(response.first_name);
+        var last = JSON.stringify(response.last_name);
+        connection.query(`DELETE FROM employee WHERE first_name = ${first} AND last_name = ${last}`,function (err){
+            if (err) {console.log(err);}
+            console.log("successfully removed");
+            connection.end();
+        })
+    })
+};
+// function addDepartment(department){
+//     connection.query("SELECT * FROM department", function(data){
+//         console.log(data)
+//     })
+//     connection.query("INSERT INTO department SET ?",
+//     {
+//         name: department
+//     },
+//     function(err, data){
+//         if (err){
+//             console.log(err)
+//         }
+//         console.log("you added a department")
+//     }
+//     )
+// }
--- seed.sql
+++ seed.sql
@@ -4,39 +4,31 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-    id INT auto_increment NOT NULL,
+    dep_id INT auto_increment NOT NULL,
     name VARCHAR(30),
-    PRIMARY KEY(id)
+    PRIMARY KEY(dep_id)
 );
 
 CREATE TABLE role (
-    id INT auto_increment NOT NULL,
+    r_id INT auto_increment NOT NULL,
     department_id INT,
     title VARCHAR(30), 
     salary DECIMAL(65,3),
-    PRIMARY KEY(id),
-    Foreign key (department_id) references department(id)
+    PRIMARY KEY(r_id),
+    Foreign key (department_id) references department(dep_id)
 );
 
 CREATE TABLE employee (
-    id INT auto_increment NOT NULL,
+    emp_id INT auto_increment NOT NULL,
     role_id INT,
     manager_id INT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    PRIMARY KEY(id),
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    PRIMARY KEY(emp_id),
+    FOREIGN KEY (role_id) REFERENCES role(r_id),
+    FOREIGN KEY (manager_id) REFERENCES employee(emp_id)
 );
 
-INSERT INTO department (name)
-VALUES ('tech');
-
-INSERT INTO role (title, salary)
-VALUES ('junior software engineer', 80.000);
-
-INSERT INTO employee (first_name, last_name)
-VALUES ('adam', 'sanchez');
 
 SELECT * FROM department;
 SELECT * FROM role;
